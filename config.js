88c16058750c8224664feed8eeb9fa1c45575f94
phamthang2003 <116180596+phamthang2003@users.noreply.github.com>
2022-10-20 20:35:08 +0700

    Add files via upload
    anhtrinhlinh

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'bạn Linhhh';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng bạn Trình Linh';
+const contentCard = 'chúc bạn Linh có 1 ngày 20/10 thật nhiều niềm vui, chúc bạn luôn có sức khỏe, luôn xinh đẹp, thành công trong công việc nhoaa'
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anhtrinhlinh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
